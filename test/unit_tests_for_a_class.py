b3829492b4574dd5dfaf1b8a7d3fccfaf4d9e32f
Kelly Klein <kmklein2@dmacc.edu>
2020-07-07 16:24:31 -0500

    1st test passed

--- test/unit_tests_for_a_class.py
+++ test/unit_tests_for_a_class.py
@@ -2,15 +2,17 @@ import unittest
 from class_definitions import student as s
 
 
-
 class MyTestCase(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         self.student = s.Student('Klein', 'Kelly', 'CIS', '3.7')
 
     def tearDown(self):
         del self.student
 
-    
+    def test_object_created_required_attributes(self):
+        self.assertEqual(self.student.last_name, 'Klein')
+        self.assertEqual(self.student.first_name, 'Kelly')
+        self.assertEqual(self.student.major, 'CIS')
 
 
 if __name__ == '__main__':
